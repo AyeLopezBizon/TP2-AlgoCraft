f9c74f5ad5786c391f8fa0211debfdec4c1e747a
Ayelen Lopez Bizon <aye.lopez.bizon@hotmail.com>
2019-06-09 01:05:01 -0300

    Clases excepcion de Durabilidad y Herramienta.

---
+++ src/fiuba/algo3/tp2/herramienta/HerramientaDesgastadaNoSePuedeUsarException.java
@@ -0,0 +1,4 @@
+package fiuba.algo3.tp2.herramienta;
+
+public class HerramientaDesgastadaNoSePuedeUsarException extends Exception {
+}
---
+++ src/fiuba/algo3/tp2/herramienta/durabilidad/DurabilidadDesgastadaNoSePuedeReducirException.java
@@ -0,0 +1,4 @@
+package fiuba.algo3.tp2.herramienta.durabilidad;
+
+public class DurabilidadDesgastadaNoSePuedeReducirException extends Exception {
+}
