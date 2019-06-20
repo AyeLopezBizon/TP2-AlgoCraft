485def66d4ea8b333d148264de150af70fd32fc5
Christian Fernández <cefernandez@fi.uba.ar>
2019-06-20 19:55:23 -0300

    Agrego transicion material golpeado

--- src/fiuba/algo3/tp2/vista/ocupanteTerreno/VistaMaterial.java
+++ src/fiuba/algo3/tp2/vista/ocupanteTerreno/VistaMaterial.java
@@ -7,16 +7,21 @@ import fiuba.algo3.tp2.modelo.material.Material;
 import fiuba.algo3.tp2.modelo.terreno.OcupanteTerreno;
 import fiuba.algo3.tp2.vista.terreno.VistaCasilleroTerreno;
 import fiuba.algo3.tp2.vista.terreno.VistaOcupanteTerreno;
+import javafx.animation.RotateTransition;
+import javafx.util.Duration;
 
 public abstract class VistaMaterial extends Vista implements VistaOcupanteTerreno {
 
 	private VistaCasilleroTerreno vistaCasilleroTerreno;
 	private Material material;
 	private boolean materialYaSeEncuentraDebilitado;
+	private RotateTransition transicionGolpe;
+	
 
 	public VistaMaterial(VistaCasilleroTerreno vistaCasilleroTerreno, OcupanteTerreno ocupanteTerreno) {
 		this.vistaCasilleroTerreno = vistaCasilleroTerreno;
 		this.material = (Material)ocupanteTerreno;
+		this.transicionGolpe = crearTransicionGolpe();
 	}
 
 	@Override
@@ -31,7 +36,7 @@ public abstract class VistaMaterial extends Vista implements VistaOcupanteTerren
 			BigDecimal durabilidad = (BigDecimal)parametros[2];
 			BigDecimal durabilidadMaxima = (BigDecimal)parametros[3];
 
-			dibujarDanio(danio);
+			transicionGolpe.play();
 			dibujarMaterialDebilitado(durabilidad, durabilidadMaxima);
 		}
 	}
@@ -49,8 +54,14 @@ public abstract class VistaMaterial extends Vista implements VistaOcupanteTerren
 		}
 	}
 
-	private void dibujarDanio(BigDecimal danio) {
-		System.out.println("REDUCCION DE DURABILIDAD EN" + danio);
+	private RotateTransition crearTransicionGolpe() {
+		
+		RotateTransition trans = new RotateTransition(Duration.seconds(0.1), vistaCasilleroTerreno);
+        trans.setFromAngle(0.0);
+        trans.setToAngle(20.0);
+        trans.setCycleCount(2);
+        trans.setAutoReverse(true);
+		return trans;
 	}
 
 	@Override
